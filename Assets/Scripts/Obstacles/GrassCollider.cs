d4ff1445ef1ecc17e59d4a53d73e7ba8ddc05deb
RandumbGuy1 <78370387+RandumbGuy1@users.noreply.github.com>
2022-07-04 11:02:15 -0400

    grass sway
    idk

---
+++ Assets/Scripts/Obstacles/GrassCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassCollider : MonoBehaviour
+{
+    [Header("Interactive Intensity")]
+    [SerializeField] private LayerMask collidesWithGrass;
+    [SerializeField] private float swayAmount;
+    [SerializeField] private float dragAmount;
+    [SerializeField] private Renderer grassRenderer;
+
+    void OnTriggerStay(Collider other)
+    {
+        int layer = other.gameObject.layer;
+        if (collidesWithGrass == (collidesWithGrass | 1 << layer)) return;
+
+        print("lmao gras toucher!");
+    }
+}
