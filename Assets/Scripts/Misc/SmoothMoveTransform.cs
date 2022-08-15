7be8f782de94c4a1f7ea0db32969b6796d3ffa47
RandumbGuy1 <78370387+RandumbGuy1@users.noreply.github.com>
2022-08-15 14:16:10 -0400

    8/15

---
+++ Assets/Scripts/Misc/SmoothMoveTransform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothMoveTransform : MonoBehaviour
+{
+    [SerializeField] private Transform affected;
+    [SerializeField] private float posSmoothTime;
+    [SerializeField] private float rotAngle;
+    private Vector3 targetPos, posVel = Vector3.zero;
+
+    void Start()
+    {
+        targetPos = affected.position;
+    }
+
+    void Update()
+    {
+        if ((targetPos - affected.position).sqrMagnitude < 0.001f) return;
+
+        affected.position = Vector3.SmoothDamp(affected.position, targetPos, ref posVel, posSmoothTime);
+        affected.Rotate(Vector3.up, rotAngle);
+    }
+
+    public void Fly(float height) => targetPos.y += height;
+}
