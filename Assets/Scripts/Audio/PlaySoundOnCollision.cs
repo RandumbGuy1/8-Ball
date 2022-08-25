32ca6128556418328c49c9b58d389d2fe64cae94
RandumbGuy1 <78370387+RandumbGuy1@users.noreply.github.com>
2022-08-25 12:38:57 -0400

    8/25
    idk

---
+++ Assets/Scripts/Audio/PlaySoundOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    [SerializeField] private AudioClip clip;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        AudioManager.Instance.PlayOnce(clip, transform.position);
+    }
+}
---
+++ Assets/Scripts/Interactions/PlankPeel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankPeel : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Rigidbody plankRb;
+    [SerializeField] private AudioClip peelClip;
+
+    public PlayerRef Player { get; set; }
+    public GameObject GameObject => gameObject;
+
+    private bool peeled = false;
+
+    public string GetDescription(PlayerRef player)
+    {
+        return peeled ? null : "Remove Plank";
+    }
+
+    public void OnEndHover(PlayerRef player)
+    {
+        
+    }
+
+    public void OnInteract(PlayerRef player)
+    {
+        AudioManager.Instance.PlayOnce(peelClip, transform.position);
+        plankRb.isKinematic = false;
+        peeled = true;
+    }
+
+    public void OnStartHover(PlayerRef player)
+    {
+        
+    }
+}
